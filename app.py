98864353fb0df591c3f8870518d351230e225aec
ursarah <sarahurcezino@gmail.com>
2025-04-11 11:25:09 -0300

    feat: preparando para deploy no rail

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +17,7 @@ def carregar_dados():
 
 def salvar_dados(dados):
     with open('database/alunos.json', 'w') as file:
-        json.dump(dados, file)
+        json.dump(dados, file, sort_keys=True)
 
 
 ALUNOS = carregar_dados()
@@ -31,25 +32,24 @@ def metodos_http():
         user = request.get_json()
 
         if user['nome'] == '' or user['email'] == '' or user['telefone'] == '' or user['bairro'] == '':
-            return Response(status=400)
+            return '', 400
         else:
+            id = random.randint(0, 999999)
             dict_aluno = {
-                "id": 1,
+                "contagem_aluno": 1,
+                "id": id,
                 "nome": user['nome'],
                 "telefone": user['telefone'],
                 "email": user['email'],
                 "bairro": user['bairro'],
             }
             for aluno in ALUNOS:
-                if aluno['id'] == dict_aluno['id']:
-                    dict_aluno['id'] += 1
+                if aluno['contagem_aluno'] == dict_aluno['contagem_aluno']:
+                    dict_aluno['contagem_aluno'] = len(ALUNOS) + 1
 
             ALUNOS.append(dict_aluno)
             salvar_dados(ALUNOS)
-            return Response(status=200)
-
-    if request.method == 'DELETE':
-        ...
+            return '', 200
 
 
 @app.route('/delete/<int:id>', methods=['DELETE'])
@@ -57,7 +57,26 @@ def delete_aluno(id):
     global ALUNOS
     ALUNOS = [aluno for aluno in ALUNOS if aluno['id'] != id]
     salvar_dados(ALUNOS)
-    return Response(200)
+    return '', (200)
+
+
+@app.route('/edit/<int:id>', methods=['PATCH'])
+def edit_aluno(id):
+    global ALUNOS
+    userBody = request.get_json()
+
+    if userBody['nome'] == '' and userBody['email'] == '' and userBody['telefone'] == '' and userBody['bairro'] == '':
+        return '', 400
+    else:
+        fields = ['nome', 'email', 'telefone', 'bairro']
+
+        for aluno in ALUNOS:
+            if aluno['id'] == id:
+                for field in fields:
+                    if userBody[field] != '':
+                        aluno[field] = userBody[field]
+        salvar_dados(ALUNOS)
+        return '', 200
 
 
 if __name__ == "__main__":
